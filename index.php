d0f9ac28e3681b74778d48c7535b159a9362a9b5
rizkyfbrnsyh88 <rizkyfebriansyah88@gmail.com>
2023-07-29 07:48:40 +0700

    Membuat Home Page

--- index.php
+++ index.php
@@ -4,9 +4,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>SPK | Analytical Hierarchy Process</title>
+    <link rel="stylesheet" href="assets/css/index.css">
 </head>
 <body>
+    <nav>
+        <div class="logo">
+            <h1>SPK</h1>
+        </div>
+        <div class="links">
+            <a href="" class="active">Home</a>
+            <a href="">Analytical Hierarchy Process</a>
+            <a href="">About</a>
+        </div>
+        <div class="sign-in">
+            <a href="">Sign In</a>
+        </div>
+    </nav>
+
+    <div class="box-content">
+        <div class="text-content">
+            <h1>Sistem Pendukung Keputusan <span>Pemilihan Guru Terbaik</span></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum facere quos eius voluptatem tempora voluptas.</p>
+        </div>
+        <div class="image-content">
+            <img src="assets/images/Decision.png" alt="Decision">
+        </div>
+    </div>
     
 </body>
 </html>
